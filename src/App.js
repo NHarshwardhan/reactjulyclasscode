ad8604c97548dc45cc42905894a6a3f4fb3ab5ae
Nishant Harshwardhan <nishantharshwardhan@gmail.com>
2024-07-29 22:14:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Greet from './components/Greet'
 
-function App() {
+export default function App() {
+   
+   let person = {
+       firstname:'Mike',
+       lastname:'Tyson'
+   }
+
+
+   let users = [
+      { id: 101, name: 'Peter', age:34},
+      { id: 102, name: 'John', age:34},
+      { id: 103, name: 'Kerry', age:34},
+   ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <>
+        {/* <Greet fname="Peter" lname="Parker" age={24}/>  
+        <Greet fname="Rose" lname="Dawson" age={25}/>  
+        <Greet fname="Kerry" lname="Parera" age={26}/>   */}
 
-export default App;
+
+        {/* <Greet personInfo = {person}/> */}
+
+        <Greet userInfo = {users}/>
+      </>
+  )
+}
